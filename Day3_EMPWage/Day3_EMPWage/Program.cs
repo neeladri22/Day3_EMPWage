8db788c365155120d13efa33079b4431eb3ab559
unknown <neeladri22@gmail.com>
2022-06-26 00:05:58 +0530

    first commit for Uc1

--- Day3_EMPWage/Day3_EMPWage/Program.cs
+++ Day3_EMPWage/Day3_EMPWage/Program.cs
@@ -10,7 +10,21 @@ namespace Day3_EMPWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to EmployeeWage Computation");
+            
+        }
+        public static void CheckEmployeePresentOrAbsent() {
+
+            Console.WriteLine("Welcome to Employee pay roll");
+            Random random = new Random();
+            int number = random.Next(0, 2);
+
+            if (number == 0)
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            else {
+                Console.WriteLine("Employee is Present");
+            }
         }
     }
 }
