edd6f9e54bd0b87551c80e2280ab4a76d01327e9
unknown <neeladri22@gmail.com>
2022-06-26 22:02:54 +0530

    Added the UC7 Refactor Program file

--- Day3_EMPWage/Day3_EMPWage/Program.cs
+++ Day3_EMPWage/Day3_EMPWage/Program.cs
@@ -8,43 +8,9 @@ namespace Day3_EMPWage
 {
     internal class Program
     {
-        public const int IsPartTime = 1;
-        public const int IsFullTime = 2;
-        public const int IsAbsent = 0;
-        public const int wagePerHour = 20;
-        public const int NumberOfWorkingDays = 20;
-        public const int MaxHoursInMonth = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculating employee Wages for a Month");
-            int empHours = 0;
-            int empwage = 0;
-            int totalWage = 0;
-           
-            for (int day=0;day<NumberOfWorkingDays;day++) 
-            {
-                Random random = new Random();
-                int number = random.Next(0, 3);
-                switch (number) 
-                {
-                case IsAbsent:
-                        empHours = 0;
-                        break;
-
-                case IsFullTime:
-                        empHours = 8;
-                        break;
-
-                case IsPartTime:
-                        empHours = 4;
-                        break;
-
-                }
-                empwage = empHours * wagePerHour;
-                totalWage = totalWage + empwage;
-                Console.WriteLine("Employee wage is :"+ empwage);
-            }
-            Console.WriteLine("total Employee wage is :" + totalWage);
+            EmployeeWage.EmployeeWages();
         }
     }
 }
