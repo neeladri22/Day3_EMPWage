09164abc403a650035c5da2ec11458d10977d1e0
unknown <neeladri22@gmail.com>
2022-06-26 13:37:06 +0530

    Added new code for UC5

--- Day3_EMPWage/Day3_EMPWage/Program.cs
+++ Day3_EMPWage/Day3_EMPWage/Program.cs
@@ -8,39 +8,45 @@ namespace Day3_EMPWage
 {
     internal class Program
     {
+        public const int IsPartTime = 1;
+        public const int IsFullTime = 2;
+        public const int IsAbsent = 0;
+
         static void Main(string[] args)
         {
-            SwitchCaseForEmployeeAndWage();
-        }
-        public static void SwitchCaseForEmployeeAndWage() {
-
-            Console.WriteLine("Add part time Employee wage");
-            Random random = new Random();
-            int number = random.Next(0, 3);
+            Console.WriteLine("Calculating the Wages for a Month");
             int wagePerHour = 20;
-            int fullyDayHour = 8;
+            int fullyTimeHour = 8;
             int partTimeHour = 4;
             int totalWage = 0;
+            int NumberOfWorkingDays = 20;
 
-            switch (number) 
+            for (int i=0;i<NumberOfWorkingDays;i++) 
             {
-                case 0: 
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                Random random = new Random();
+                int number = random.Next(0, 3);
+                switch (number) 
+                {
+                    case IsAbsent:
+                      Console.WriteLine("Employee is Absent");
+                break;
 
-                case 1:
-                    Console.WriteLine("Employee is Present");
-                    totalWage = wagePerHour * fullyDayHour;
-                    Console.WriteLine("Full time Employee's total wage is :" + totalWage);
-                    break;
+                case IsFullTime:
+                    Console.WriteLine("Employee is fulltime");
+                totalWage =totalWage + wagePerHour * fullyTimeHour;
+                Console.WriteLine("Full time Employee's total wage is :" + totalWage);
+                break;
 
-                case 2:
+                case IsPartTime:
                     Console.WriteLine("Employee is Present and Part time Employee");
-                    totalWage = wagePerHour * partTimeHour;
-                    Console.WriteLine("Part time Employee's total wage is :" + totalWage);
-                    break;
+                totalWage = totalWage + wagePerHour * partTimeHour;
+                Console.WriteLine("Part time Employee's total wage is :" + totalWage);
+                break;
+
             }
-       
+
+            }               
+
         }
     }
 }
