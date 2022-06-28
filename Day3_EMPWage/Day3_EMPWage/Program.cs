fe524ecbd8306a7a8cb03ff03dd4aa47e1ca7544
unknown <neeladri22@gmail.com>
2022-06-28 17:13:45 +0530

    Added UC9 program code

--- Day3_EMPWage/Day3_EMPWage/Program.cs
+++ Day3_EMPWage/Day3_EMPWage/Program.cs
@@ -8,43 +8,15 @@ namespace Day3_EMPWage
 {
     internal class Program
     {
-        public const int fullTime = 1;
-        public const int partTime = 2;
-
-        public static int computeEmpWage(string company, int empRatePerHour, int MaximumHoursPerMonth, int numOfWorkingDays)
-        {
-            int empHrs = 0;
-            int totalWorkingDays = 0;
-            int totalEmpHrs = 0;
-            while (totalEmpHrs <= MaximumHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case fullTime:
-                        empHrs = 8;
-                        break;
-                    case partTime:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs = totalEmpHrs + empHrs;
-                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Emp wage for company : " + company + " is: " + totalEmpWage);
-            return totalEmpWage;
-
-        }
         static void Main(string[] args)
         {
-            computeEmpWage("Flipcart", 20,2,10);
-            computeEmpWage("Dmart", 10, 4, 20);
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+
         }
     }
 }
